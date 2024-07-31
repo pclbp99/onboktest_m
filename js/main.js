dc400df4193e67ee0dffed9d389a9005fa714c52
pclbp99 <71331585+pclbp99@users.noreply.github.com>
2024-07-31 17:37:43 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
     $('header').click(function(){
         $('.menu_pop').fadeIn();
+        $('.body').addClass('blur');
         $('.menu_pop').click(function(){
             $('.menu_pop').fadeOut();
+            $('.body').removeClass('blur');
         })
     });
 });
